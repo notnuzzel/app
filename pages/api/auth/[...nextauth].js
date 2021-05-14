bb046664af6b185aff342f8b67b03c3a5b7dcdfc
Daniel Sinclair <d@niel.nyc>
2021-05-14 02:08:56 -0400

    migrated to @nuzzel/prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,9 +2,7 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 import Adapters from 'next-auth/adapters'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import prisma from '@notnuzzel/prisma'
 
 export default NextAuth({
   providers: [
